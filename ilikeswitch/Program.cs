2018b179dce516130fe68c31278ac905a7b7fb6d
yunwhite <s2018s12@e-mirim.hs.kr>
2020-05-06 23:23:56 +0900

    초기 커밋.

---
+++ ilikeswitch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ilikeswitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("학년을 입력하세요: ");
+            int input = int.Parse(Console.ReadLine());
+            switch (input)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("겨울입니다");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄입니다");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름입니다");
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("가을입니다");
+                    break;
+                default:
+                    Console.WriteLine("잘못 입력했습니다");
+                    break;
+            }
+        }
+    }
+}
